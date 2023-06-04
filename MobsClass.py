b7904d4119b7ec980e6a70943fcbcd7381e09015
TerryW624 <terrywade634@gmail.com>
2023-06-04 16:51:22 -0700

    Feat: Character Classes Part 1

---
+++ MobsClass.py
@@ -0,0 +1,23 @@
+from character_file import Mobs
+
+class Undead(Mobs):
+    def __init__(self, name):
+        super().__init__(name)
+
+class DragonRogue(Mobs):
+    def __init__(self, name):
+        super().__init__(name)
+
+class DragonCultist(Mobs):
+    def __init__(self, name):
+        super().__init__(name)
+
+class Chicken(Mobs):
+    def __init__(self, name):
+        super().__init__(name)
+
+class Hawk(Mobs):
+    def __init__(self, name):
+        super().__init__(name)
+
+class 
---
+++ character_file.py
@@ -0,0 +1,28 @@
+class Character:
+    def __init__(self, name):
+        self.name = name
+
+class Hercules(Character):
+    def __init__(self, name):
+        super().__init__(name)
+
+class Mobs(Character):
+    def __init__(self, name):
+        super().__init__(name)
+
+class Margwa(Character):
+    def __init__(self, name):
+        super().__init__(name)
+
+class NemeanLion(Character):
+    def __init__(self, name):
+        super().__init__(name)
+
+class LernaeanHydra(Character):
+    def __init__(self, name):
+        super().__init__(name)
+
+class StymphalianBird(Character):
+    def __init__(self, name):
+        super().__init__(name)
+
