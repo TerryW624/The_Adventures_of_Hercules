c6b6bf45cec85ecb6d5d9db743ac0e53c3c92d2b
TerryW624 <terrywade634@gmail.com>
2023-06-05 12:10:30 -0700

    Set up Entities of the labors

---
+++ ItemsClass.py
@@ -0,0 +1,23 @@
+class Items:
+    pass
+
+class Armor:
+    pass
+
+class Weapon:
+    pass
+
+class QuestItems(Items):
+    pass
+
+class QuestAnimal(QuestItems):
+    pass
+
+class CeryneianHind(QuestAnimal):
+    pass
+
+class ErymanthianBoar(QuestAnimal):
+    pass
+
+class CretanBull(QuestAnimal):
+    pass
